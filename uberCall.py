00d0888141538fe325ade78bba6b6d4e36566e98
HarryPahwa <harrypahwa95@gmail.com>
2016-09-17 23:13:27 -0400

    Got Price Estimate
    Saves to notepad file

--- uberCall.py
+++ uberCall.py
@@ -9,7 +9,37 @@ print("this ran")
 def returnProducts(): 
 	print('~~~~~~~~~~~~~')
 	print(products)
+	text_file = open("Output.txt", "w")
+	for item in products:
+  		text_file.write("%s\n" % item)
+	text_file.close()
 	print('~~~~~~~~~~~~~')
+
+
+def returnPriceEstimate():
+	import requests
+
+	url = 'https://api.uber.com/v1/estimates/price'
+
+	parameters = {
+    'server_token': '60KIlvmpVK8JWNiNvau69NTqziJsT-0KM9e0Hlfb',
+    'start_latitude': 37.775818,
+    'start_longitude': -122.418028,
+	'end_latitude': 37.175818,
+    'end_longitude': -123.418028,
+	}
+
+	response = requests.get(url, params=parameters)
+
+	data = response.json()
+	print(data)
+	text_file = open("Output.txt", "w")
+	for item in products:
+  		text_file.write("%s\n" % item)
+	text_file.close()
+	print('~~~~~~~~~~~~~')
+
+
 
 
 # client = UberRidesClient(session, sandbox_mode=True)
@@ -28,7 +58,7 @@ def returnProducts():
 def getAuthenticationURL(): 
 	from uber_rides.auth import AuthorizationCodeGrant
 	auth_flow = AuthorizationCodeGrant(
-	    'Xy07019vgK4SdQGGxPYFOjUVBRbskX0Z',
+	    'Xy07019vgK4SdQGGxPYFOjUVBRbskX0Z',	
 	    ['profile'],
 	    'cgN1x77XOhdLP7LADi1jO0CARk5ce9v11T8ALtZ9',
 	    'http://localhost:5000/callback',
